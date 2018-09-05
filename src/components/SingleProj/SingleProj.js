71279f7ec84a16560840b259d3914c595aa92145
Becca Bae <rebekahlbae@gmail.com>
2018-09-05 18:34:03 -0400

    Single project page view renders title, services, desc, and image

--- src/components/SingleProj/SingleProj.js
+++ src/components/SingleProj/SingleProj.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
+import ToolButton from './ToolButton/ToolButton';
+import './SingleProj.css';
 
 class SingleProj extends Component {
 	render() {
+
 		const id = this.props.match.params.id;
 		const p = this.props.work[id];
+		const tools = p.tools.map((t, index) => {
+			return (<ToolButton key={index} label={t}/>)
+		})
+
 		return (
-			<div>
-			{p.title}
+			<div className="single-proj-container">
+			<p id="proj-title">{p.title}</p>
+			{p.services && (<small id="services">{(p.services).join(', ')}</small>)}
 			<br/>
-			{p.description}
+			<p id="proj-desc">{p.description}</p>
 			<br/>
-			{p.tools}
-			<br/>
-			{p.services}
+			<img src={`${p.src}`}/>
 			</div>
 		);
 	}
