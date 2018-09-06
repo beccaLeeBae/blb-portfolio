58f98faa37e3de2c81f37479432f75748d51e992
Becca Bae <rebekahlbae@gmail.com>
2018-09-05 21:28:01 -0400

    Project grid hover effect to display project title

---
+++ src/components/SingleProj/ProjTools/ProjTools.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ToolButton from '../ToolButton/ToolButton';
+import "./ProjTools.css";
+
+class ProjTools extends Component {
+	render() {
+		const tools = (this.props.p).tools.map((t, index) => {
+			return <ToolButton key={index} label={t} />;
+		});
+		return (
+			<div className="proj-tools">{tools}</div>
+		);
+	}
+}
+
+export default ProjTools;
--- src/components/SingleProj/SingleProj.js
+++ src/components/SingleProj/SingleProj.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
-import ToolButton from './ToolButton/ToolButton';
-import './SingleProj.css';
+import ProjTools from "./ProjTools/ProjTools";
+import SiteButton from "./SiteButton/SiteButton";
+import "./SingleProj.css";
 
 class SingleProj extends Component {
+	componentDidMount() {
+		window.scrollTo(0, 0);
+	}
 	render() {
-
 		const id = this.props.match.params.id;
 		const p = this.props.work[id];
-		const tools = p.tools.map((t, index) => {
-			return (<ToolButton key={index} label={t}/>)
-		})
 
 		return (
 			<div className="single-proj-container">
-			<p id="proj-title">{p.title}</p>
-			{p.services && (<small id="services">{(p.services).join(', ')}</small>)}
-			<p id="proj-desc">{p.description}</p>
-			<img src={`${p.imgLg}`}/>
-			<div className="proj-tools">
-			{tools}
-			</div>
-			<br/>
-			{p.link && (<a href={p.link} target="_blank" rel="noopener noreferrer"><button id="view-proj-btn">View Site</button></a>)}
-			<br/>
-			<a id="back-btn" href="/">← BACK</a>
+				<p id="proj-title">{p.title}</p>
+				{p.services && <small id="services">{p.services.join(", ")}</small>}
+				<p id="proj-desc">{p.description}</p>
+				<img src={`${p.imgLg}`} />
+				<ProjTools p={p}/>
+				<br />
+				{p.link && <SiteButton link={p.link} />}
+				<br />
+				<a id="back-btn" href="/">
+					← BACK
+				</a>
 			</div>
 		);
 	}
